1ecefb28e973fdd7116acbe1c65b794a4b4e4256
0qy <fordnicholasjones@gmail.com>
2023-03-10 14:52:39 +1300

    update form onchange and add button

--- client/components/subcomponents/BookingForm.jsx
+++ client/components/subcomponents/BookingForm.jsx
@@ -4,36 +4,43 @@ export default function BookingForm() {
     const [bookingForm, setBookingForm] = useState({first_name: "", last_name: "", pref_name: "", email: "", mobile_number: "", pref_pronoun: "", flash: false, custom: false, brief_description: "", pref_day: "", pref_time: "", any_questions: ""})
 
     function handleChange(e) {
+        if(e.target.type == "checkbox") {
+            setBookingForm({...bookingForm, [e.target.name]: true})
+        }
+        if(e.target.type == "text") {
+            setBookingForm({ ...bookingForm, [e.target.name]: e.target.value })
+        }
+    }
+
+    function handleClick(e) {
         e.preventDefault()
+        console.log(bookingForm)
     }
 
     return(
         <>
-
-        <form className="bookingForm" noValidate>
+        <form id="bookingForm" >
         <h3>Booking Details:</h3>
-            <label htmlFor="firstName" className="label">First Name:</label>
+            <label htmlFor="first_name" className="label">First Name:</label>
             <input 
             type="text" 
-            name="firstName" 
-            placeHolder=" " 
+            name="first_name"  
             value={bookingForm.first_name} 
             onChange={handleChange}
             className="textbox"
             ></input>
-            <label htmlFor="lastName" className="label">Last Name:</label>
+            <label htmlFor="last_name" className="label">Last Name:</label>
             <input
             type="text"
-            name="lastName"
-            placeholder=" "
+            name="last_name"
             value={bookingForm.last_name}
             onChange={handleChange}
             className="textbox"
             ></input>
-            <label htmlFor="prefName" className="label">Preffered Name:</label>
+            <label htmlFor="pref_name" className="label">Preffered Name:</label>
             <input
             type="text"
-            name="prefName"
+            name="pref_name"
             placeholder="(optional)"
             value={bookingForm.pref_name}
             onChange={handleChange}
@@ -43,26 +50,25 @@ export default function BookingForm() {
             <input
             type="text"
             name="email"
-            placeholder=" "
             value={bookingForm.email}
             onChange={handleChange}
             className="textbox"
             ></input>
-            <label htmlFor="mobileNumber" className="label">Phone Number:</label>
+            <label htmlFor="mobile_number" className="label">Phone Number:</label>
             <input
             type="text"
-            name="mobileNumber"
-            placeholder=" "
+            name="mobile_number"
             value={bookingForm.mobile_number}
             onChange={handleChange}
             className="textbox"
             ></input>
-            <label htmlFor="prefPronouns" className="label">Preffered Pronouns:</label>
+            <label htmlFor="pref_pronoun" className="label">Preffered Pronouns:</label>
             <input
             type="text"
-            name="prefPronouns"
+            name="pref_pronoun"
             placeholder="(optional)"
             value={bookingForm.pref_pronoun}
+            onChange={handleChange}
             className="textbox"
             ></input>
 
@@ -83,10 +89,10 @@ export default function BookingForm() {
             value={bookingForm.custom}
             onChange={handleChange}
             ></input>
-            <label htmlFor="briefDescription" className="label">Give a Description:</label>
+            <label htmlFor="briefe_dscription" className="label">Give a Description:</label>
             <input
             type="text"
-            name="briefDescription" 
+            name="brief_description" 
             placeholder="Anything you'd like; placement, colors, size etc..."
             value={bookingForm.brief_description}
             onChange={handleChange}
@@ -95,20 +101,19 @@ export default function BookingForm() {
             
         <h3>Dates & Times:</h3>
                         
-            <label htmlFor="prefDays" className="label">Preffered Days:</label>
+            <label htmlFor="pref_day" className="label">Preffered Days:</label>
             <input 
             type="text"
-            name="prefDays"
+            name="pref_day"
             placeholder="what works for you."
             value={bookingForm.pref_day}
             onChange={handleChange}
             className="textbox"
             ></input>
-            <label htmlFor="prefTime"className="label">Preffered Times:</label>
+            <label htmlFor="pref_time"className="label">Preffered Times:</label>
             <input
             type="text"
-            name="prefTime"
-            placeholder=" "
+            name="pref_time"
             value={bookingForm.pref_time}
             onChange={handleChange}
             className="textbox"
@@ -116,14 +121,17 @@ export default function BookingForm() {
         
         <h3>Any further questions are welcomed:</h3>
 
-            <label htmlFor="anyQuestions" className="label">Any Questions?</label>
+            <label htmlFor="any_questions" className="label">Any Questions?</label>
             <input
             type="text"
-            name="anyQuestions"
+            name="any_questions"
             placeholder="(optional)"
             value={bookingForm.any_questions}
+            onChange={handleChange}
             className="bigTextBox"
             ></input>
+
+            <button type="submit" onClick={handleClick} className="button">Submit your booking</button>
         </form>
         </>
     )
