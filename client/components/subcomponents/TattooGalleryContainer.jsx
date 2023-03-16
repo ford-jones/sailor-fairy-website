f6da8b234c4acfae2a9c1f43ac08326fd3144609
Ford Jones <fordnicholasjones@gmail.com>
2023-03-16 17:26:38 +1300

    add back button

--- client/components/subcomponents/TattooGalleryContainer.jsx
+++ client/components/subcomponents/TattooGalleryContainer.jsx
@@ -1,64 +1,75 @@
-import React, {useState, useMemo} from 'react'
+import React, { useState, useMemo } from 'react'
 
-import {fetchGallery} from '../../api/tattooGallery'
+import { fetchGallery } from '../../api/tattooGallery'
+import GoBack from './GoBack'
 import Loader from './Loader'
 
 export default function TattooGalleryContainer() {
-    const [gallery, setGallery] = useState([])
-    const [loading, setLoading] = useState(true)
-    
-    const allImages = document.getElementsByTagName('img')
-    const images =[...allImages]
+  const [gallery, setGallery] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [backArrow, setBackArrow] = useState(false)
 
-    useMemo(async () => {
-        const getGallery = await fetchGallery()
-        const reverseGallery = getGallery.images.reverse()
+  const allImages = document.getElementsByTagName('img')
+  const images = [...allImages]
 
-        setGallery(reverseGallery)
+  useMemo(async () => {
+    const getGallery = await fetchGallery()
+    const reverseGallery = getGallery.images.reverse()
 
-        setTimeout(() => {
-            setLoading(false)    
-        }, 2000);
-        
-    }, [loading])
+    setGallery(reverseGallery)
 
-    function onClick(e) {
-        e.preventDefault()
-        
-        const getContainer = document.getElementsByClassName('imageContainer')
-        const container = [...getContainer]
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [loading])
 
-            container[0].style.display = 'flex'
-            container[0].style.flexDirection = 'column'
-            
-            for(let i = 0; i < images.length; i++) {
-                const image = images[i]
-                if(image.id) {
-                    image.style.width = '800px'
-                    image.style.height = '800px'
-                    image.style.marginLeft = 'auto'
-                    image.style.marginRight = 'auto'
-                    image.style.position = 'relative'
-                }
-            }
-            const foundImage = images.find((x) => {
-                return x.id === e.target.id
-            })
-            foundImage.scrollIntoView({block: 'center'})
-        
+  function onClick(e) {
+    e.preventDefault()
 
+    const getContainer = document.getElementsByClassName('imageContainer')
+    const container = [...getContainer]
+
+    container[0].style.display = 'flex'
+    container[0].style.flexDirection = 'column'
+
+    for (let i = 0; i < images.length; i++) {
+      const image = images[i]
+      if (image.id) {
+        image.style.width = '800px'
+        image.style.height = '800px'
+        image.style.marginLeft = 'auto'
+        image.style.marginRight = 'auto'
+        image.style.position = 'relative'
+      }
     }
+    const foundImage = images.find((x) => {
+      return x.id === e.target.id
+    })
+    foundImage.scrollIntoView({ block: 'center' })
+    setBackArrow(true)
+  }
 
-    return (
-        <>
-        <div className='imageContainer'>
-            {loading  
-            ? <Loader /> 
-            : gallery.map((x) => (
-                <img src={`images/tattoos/${x.Filename}`} alt="galleryImage" className='flashImage' id={x.id} onClick={onClick}/>
-            ))
-            } 
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="imageContainer">
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            {backArrow ? <GoBack setBackArrow={setBackArrow} /> : null}
+            {gallery.map((x) => (
+              <img
+                src={`images/tattoos/${x.Filename}`}
+                alt="galleryImage"
+                className="flashImage"
+                id={x.id}
+                key={x.id}
+                onClick={onClick}
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </>
+  )
 }
